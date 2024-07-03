877c933d19a841950bf2498de490301d666b5a72
Dartanner <alun.powell.uni@googlemail.com>
2024-07-03 21:20:51 +0100

    initial commit

---
+++ YOH Minis Costing Calculator/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace YOH_Minis_Costing_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Resin (£)";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(56, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Time (£)";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 114);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Consumables (£)";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(43, 149);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Margin (%)";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(56, 184);
+            label5.Name = "label5";
+            label5.Size = new Size(47, 15);
+            label5.TabIndex = 4;
+            label5.Text = "VAT (%)";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(111, 50);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(111, 83);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(111, 114);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 7;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(111, 151);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 8;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(111, 182);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 9;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(39, 242);
+            label6.Name = "label6";
+            label6.Size = new Size(134, 15);
+            label6.TabIndex = 10;
+            label6.Text = "Recommended Price (£)";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(35, 265);
+            label7.Name = "label7";
+            label7.Size = new Size(122, 15);
+            label7.TabIndex = 11;
+            label7.Text = "Price will appear here ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label6;
+        private Label label7;
+    }
+}
---
+++ YOH Minis Costing Calculator/Form1.cs
@@ -0,0 +1,130 @@
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace YOH_Minis_Costing_Calculator
+{
+    public partial class Form1 : Form
+    {
+        //declear vereable before methods
+        private float resinCost = 1;
+        private float timeCost = 1;
+        private float consumablesCost = 1;
+
+        private int margin = 50;
+        private int VAT = 20;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label7.Text = String.Empty;
+            textBox1.Text = resinCost.ToString();
+            textBox2.Text = timeCost.ToString(); 
+            textBox3.Text = consumablesCost.ToString(); 
+            textBox4.Text = margin.ToString();
+            textBox5.Text = VAT.ToString();
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            HandleFloatForm(sender, ref resinCost);
+        }
+        private float GetPercentage(float val)
+        {
+            float percent = val + 100f;
+            percent /= 100f;
+            return percent;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            HandleFloatForm(sender, ref timeCost);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+            HandleFloatForm(sender, ref consumablesCost);
+
+        }
+
+        private void HandleFloatForm(object sender, ref float value)
+        {
+
+            TextBox text = sender as TextBox;
+
+            string val = text.Text;
+
+            if (!float.TryParse(val, out value))
+            {
+                //Debug.WriteLine ("the text isn't a float");
+                text.Text = string.Empty;
+            }
+            EstimatePrice();
+           
+        }
+
+        private void EstimatePrice()
+        {
+            float estimatedPrice = 0;
+            float baseCost = resinCost + timeCost + consumablesCost;
+
+            //float marginPercent = margin + 100f;
+            //marginPercent /= 100f;
+
+            //float VATPercent = VAT + 100f;
+            //VATPercent /= 100f;
+
+            // your a twat alun
+            // why type it five times 
+
+            float marginPercent = GetPercentage(margin);
+            float VATPercent = GetPercentage(VAT);
+
+            estimatedPrice = baseCost * marginPercent * VATPercent;
+            label7.Text = estimatedPrice.ToString("£##.##");
+        }
+
+        private void HandleIntForm(object sender, ref int value)
+        {
+            //Convert the "sender" object to the TextBox control, so we can get the value out of it
+            TextBox txt = sender as TextBox;
+
+            //Just in case - check to make sure the conversion worked
+            if (txt == null) return;
+
+            //Get the text value from the Textbox into a variable that we can use
+            string val = txt.Text;
+
+            //we need to check if the value passed into the text box is a number and if so, convert it to a float.
+            //Try parse returns a bool representing whether the parsing was a success.
+            //If successful it puts the result into the variable after "out" (resinCost)
+            if (!int.TryParse(val, out value))
+            {
+                //Using the ! (not) operator we've determined that the text string can not be turned into a float
+
+                //We have an invalid value for resin cost so set it to zero.
+                value = 0;
+                //Set the value of the Textbox to an empty string.
+                txt.Text = string.Empty;
+            }
+
+            EstimatePrice();
+
+            string trimmed = val.TrimStart('0');
+            if (!val.Equals(trimmed))
+            {
+                txt.Text = trimmed;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            HandleIntForm(sender, ref margin);
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            HandleIntForm(sender, ref VAT);
+        }
+    }
+}
